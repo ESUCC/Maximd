34045f06cab13551a34001f1cf344070a7b9e369
mdanahy <mdanahy@esucc.org>
2017-08-04 16:15:39 -0500

    More protection on Student Teams also, got rid of the first page list

--- application/controllers/StaffController.php
+++ application/controllers/StaffController.php
@@ -455,9 +455,14 @@ class StaffController extends Zend_Controller_Action
      $district_sv = $this->getRequest()->getParam('id_district');
      $school_sv=$this->getRequest()->getParam('id_school');
      
+     
+     
      $menuLink=false;
      if($studentId==null ) $menuLink=true;
      $this->view->menuLink=$menuLink;
+     
+     
+     
   
    
    //  $this->writevar1($school_sv,'this is the school in staffcontroller');
@@ -493,7 +498,9 @@ class StaffController extends Zend_Controller_Action
        $nameStudentFull= $studentName->fetchRow($studentName->select()
                                      ->where('id_student =?',$studentId));
        $this->view->nameStudentFull=$nameStudentFull['name_first']." ".$nameStudentFull['name_last'];
-      $this->writevar1($this->view->nameStudentFull,'this is the name of the student');
+       $this->view->idStudentFull=$nameStudentFull['id_student'];
+       $this->writevar1($nameStudentFull['id_student'],'this is the id of the student');
+      
     //   
                 
                 
@@ -535,7 +542,7 @@ class StaffController extends Zend_Controller_Action
           
          //  writevar($studentData[0]['name_first'],'this is the student name');
            $this->view->nameStudentFull=$nameStudent;
-           $this->writevar1($nameStudent,'this is another full name of  the student');
+         //  $this->writevar1($nameStudent,'this is another full name of  the student');
             $this->view->user_id=$user_id; 
             $this->view->studentId=$studentId;
             $this->view->studentId=$studentData[0]['id_student'];
@@ -571,6 +578,10 @@ class StaffController extends Zend_Controller_Action
         $case_mgr=false;
         $allowView=false;
         $this->view->case_mgr=false;
+        
+        // Mike added 8-4-2017 so that case managers dont see any staff for a student at the
+        // top of the Student Team.  
+        $this->view->initialList=true;
         
         foreach($_SESSION["user"]["user"]->privs as $privs ) {
             
@@ -612,23 +623,27 @@ class StaffController extends Zend_Controller_Action
                 if ($stuList['id_case_mgr']==$_SESSION['user']['id_personnel']) {
                     $studentListCaseMgr[$x]=$stuList;
                     $x=$x+1;
+                    
                    // $this->writevar1($stuList,'this is the student list');
                 }
+            
             }
             $studentList=$studentListCaseMgr;
-          //  $this->writevar1($studentListCaseMgr,'list of students for the case manager');
-       
-          
+           $this->view->studentList=$studentList;
+         //   $this->writevar1($studentList,'this is the student list');
           //  $this->view->nameStudentFull=$studentList[0]['name_first']." ".$studentList[0]['name_last'];
           
-            $listing=true;
+            // Mike added this 8-4-2017 so that nothing shows up when teachers click the
+            // Students Team button from the nav menu
+            
+            
             if($menuLink==true) {
-                $this->view->nameStudentFull="Please Select a Student to the left!";
-                $listing=false;
+                $this->view->nameStudentFull="Please Select a Student!";
+                $this->view->initialList=false;
                 
             }
                 
-        $this->view->initialListing=$listing;
+        
         
         }
         
