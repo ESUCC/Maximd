264524b71f8221ce6728481ff2d82b8f16a3e499
mdanahy <mdanahy@esucc.org>
2017-09-05 10:08:31 -0500

    Domain name change to unl.edu

--- application/controllers/StaffController.php
+++ application/controllers/StaffController.php
@@ -1061,7 +1061,7 @@ public function addotherstaffsaveAction() {
             
             
              echo "<br><br><br><center>Saved</center>";
-             echo '<center><a href="https://iepweb02.esucc.org/personnelm/edit/id_personnel/'.$request->id_personnel.'">Saved Privileges--Click to Continue</a>';
+             echo '<center><a href="https://iepweb02.unl.edu/personnelm/edit/id_personnel/'.$request->id_personnel.'">Saved Privileges--Click to Continue</a>';
            // $this->_redirect('https://iepweb02.unl.edu/personnelm/edit/id_personnel/'.$request->id_personnel);
            }
         if($okToSave==false) echo "<br><br><br><center><font color=\"red\">You do not have the 
