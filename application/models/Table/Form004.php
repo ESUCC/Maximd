b91313d5d97d98372ee901ab724981990561af40
mdanahy <mdanahy@esucc>
2018-03-13 05:06:27 -0500

    In order to get the p7 from error out when finalizing the iep we put a new function called pwnUpdate in /model/Table/Form004.  This will fill in hte pwn_decribe_action,pwn_describe_reason etc... with a value of EMPTY1

--- application/models/Table/Form004.php
+++ application/models/Table/Form004.php
@@ -2,9 +2,9 @@
 
 /**
  * Form004
- *  
+ *
  * @author jlavere
- * @version 
+ * @version
  */
 
 class Model_Table_Form004 extends Model_Table_AbstractIepForm
@@ -15,7 +15,7 @@ class Model_Table_Form004 extends Model_Table_AbstractIepForm
     protected $regEdTime;
 
     protected $_dependentTables = array(
-				'Model_Table_Form004TeamMember', 
+				'Model_Table_Form004TeamMember',
 				'Model_Table_Form004TeamOther',
 				'Model_Table_Form004TeamDistrict',
 				'Model_Table_Form004Goal',
@@ -29,23 +29,42 @@ class Model_Table_Form004 extends Model_Table_AbstractIepForm
                 'Model_Table_Form004AccomodationsChecklist',
                 'Model_Table_Form010',
     );
-    
+
+
+    // Mike added this 3-12-2018 so that pwn would work correctly
+    public function pwnUpdate($data) {
+
+        $dataSelect=array(
+            'pwn_describe_action'=>$data['pwn_describe_action'],
+            'pwn_describe_reason'=>$data['pwn_describe_reason'],
+            'pwn_options_other'=>$data['pwn_options_other'],
+            'pwn_other_factors'=>$data['pwn_other_factors'],
+            'pwn_justify_action'=>$data['pwn_justify_action']
+        );
+
+        $idForm=$data['id_form_004'];
+        $where =  "id_form_004 = '$idForm' ";
+        $this->update($dataSelect,$where);
+
+    }
+
+
     // Mike added 10-17-2017 in order to get csv to work
     public function getMostRecentIepState($id_student){
-         
+
         $sql="select * from iep_form_004 where id_student='$id_student' and status='Final' order by date_conference DESC";
         $forms=$this->db->fetchAll($sql);
         if (!empty($forms)) {
-    
+
             return $forms;
         }
         else {
             return null;
         }
-         
+
     }   // end of the function
-    
-    
+
+
     function dupe($document) {
 
 
@@ -71,12 +90,12 @@ class Model_Table_Form004 extends Model_Table_AbstractIepForm
         	return false;
         }
     }
-    
+
     function removeDupeMenu($id_student){
         $stmt = $this->db->query("SELECT status from iep_form_004 where id_student= ?",$id_student);
         $result=$stmt->fetchAll();
         $t_f=true;
-    
+
         foreach($result as $reslt){
             //      $this->writevar1($reslt['status'],'this is the result');
             if ($reslt['status']!='Final'){
@@ -85,11 +104,11 @@ class Model_Table_Form004 extends Model_Table_AbstractIepForm
         }
         // $this->writevar1($t_f,'this is the boolean in the model returned');
         return $t_f;
-    
-         
+
+
     }
-    
-    
+
+
     function buildFteMinutes($iep)
     {
         $this->spEdTime = 0;
